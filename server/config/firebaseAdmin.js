4ea7758dc7a94fac0c28904c939f2fdaabb3d832
RahulGurav82 <rr3342849@gmail.com>
2025-09-19 17:20:19 +0530

    path change

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,6 +1,16 @@
 const admin = require("firebase-admin");
+const path = require("path");
+const fs = require("fs");
+let serviceAccountPath = "/etc/secrets/serviceAccountKey.json";
 
-const serviceAccount = require("./etc/secrets/serviceAccountKey.json");
+// const serviceAccount = require("./etc/secrets/serviceAccountKey.json");
+
+// Fallback for local dev
+if (!fs.existsSync(serviceAccountPath)) {
+  serviceAccountPath = path.join(__dirname, "../serviceAccountKey.json");
+}
+
+const serviceAccount = require(serviceAccountPath);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
