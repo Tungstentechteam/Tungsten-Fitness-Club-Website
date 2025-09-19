bb65ac7a0330ae096c4c804517c99b1f9fe52242
RahulGurav82 <rr3342849@gmail.com>
2025-09-19 17:33:17 +0530

    routes added

--- server/routes/formRoutes.js
+++ server/routes/formRoutes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { saveForm } = require("../controllers/formController");
-
+const multer = require("multer");
+const {
+  saveForm,
+  saveFranchiseForm,
+  saveCareerForm,
+} = require("../controllers/formController");
+const upload = multer({ storage: multer.memoryStorage() });
 // POST /api/forms/ulwe
 router.post("/ulwe", saveForm);
-
+// POST /api/forms/franchise
+router.post("/franchise", saveFranchiseForm);
+//POST /api/forms/career
+router.post("/career", upload.single("resume"), saveCareerForm);
 module.exports = router;
